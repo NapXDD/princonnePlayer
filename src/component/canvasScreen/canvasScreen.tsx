77218936463099ef867a19e53dd1ae4e3c0b1304
Đỗ Lê Anh Khoa <khoadole21@gmail.com>
2023-10-31 16:04:07 +0700

    fix

--- src/component/canvasScreen/canvasScreen.tsx
+++ src/component/canvasScreen/canvasScreen.tsx
@@ -1,7 +1,3 @@
-import { useEffect } from "react";
-
 export default function CanvasScreen() {
-  useEffect(() => {}, []);
-
   return <canvas id="player" className="w-[100%] h-[100%]"></canvas>;
 }
